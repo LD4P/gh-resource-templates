be8ad29d537dbb872e24ce11c8eb45f5c5d4e0f7
Jeremy Nelson <jermnelson@gmail.com>
2019-02-26 16:34:19 -0700

    Initial commit for retrieving JSON from Github

---
+++ src/index.js
@@ -0,0 +1,37 @@
+
+require('dotenv').config()
+const Octokit = require('@octokit/rest')
+const SinopiaServer = require('sinopia_server')
+
+const octokit = new Octokit ({
+  auth: `token ${process.env.GITHUB_TOKEN}`
+})
+
+// import { ApiClient, LDPContainer, Resource } from 'sinopia_server'
+
+
+const main = async () => {
+  const sample_profiles = await octokit.repos.getContents(
+    { owner: 'LD4P',
+      repo: 'sinopia_sample_profiles',
+      path: '/profiles/v0.1.0/' })
+  sample_profiles.data.forEach(async row => {
+    if (row.path.endsWith('.json')) {
+      // console.log(`${row.name} - ${row.sha}`)
+      let payload = await octokit.git.getBlob({
+        owner: 'LD4P',
+        repo: 'sinopia_sample_profiles',
+        file_sha: row.sha
+      })
+      // console.log(Object.keys(payload.data))
+      let raw_json = new Buffer(payload.data.content, 'base64').toString('ascii')
+      console.log(raw_json)
+      console.log(`Finished ${row.name} length ${raw_json.length}`)
+      // let resource = new SinopiaServer.Resource(
+      //   '@graph': raw_json
+      // )
+    }
+  })
+}
+
+main()
